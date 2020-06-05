82cb2feef34116aa0c0cb28e0830e4fc4ce4479d
https://github.com/Fajar-24/DARKFB <65275087+Fajar-24@users.noreply.github.com>
2020-06-05 19:14:49 +0700

    Create WhatSapp.sh

---
+++ WhatSapp.sh
@@ -0,0 +1,88 @@
+
+
+#!/usr/bin/sh
+# coding=utf-8
+clear
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+clear
+echo $i "
+██████╗ █████╗ ██████╗  █████╗ ██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗
+███████╗███████║██║  ██║███████║██████╔╝
+╚════██║██╔══██║██║  ██║██╔══██║██╔═══╝
+███████║██║  ██║██████╔╝██║  ██║██║
+╚══════╝╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═╝╚═╝FAJAR CYBER
+"
+echo "##########################################"|lolcat
+echo "AUTHOR  : FAJAR CYBER"|lolcat
+echo "WHATSAP : 08817805411"|lolcat
+echo "GITHUB  : GITHUB.COM/FAJAR-24"|lolcat
+echo " JANGAN DI RECODE YA NJINK "|lolcat
+echo "##########################################"|lolcat
+echo "HACK CHATAN PACAR"|lolcat
+echo ""
+echo -n $cy"Ini Isi Nomor PACAR KAMU:"
+read tr
+echo "============================" | lolcat
+echo -n $cy"Nah ini isi nomor lu : "|lolcat
+read tr
+echo "============================" | lolcat
+echo -n $cy"Nah ini isi nomor lu : "|lolcat
+read lu
+echo "*SABAR TOD SEDANG MERETAS CHATAN TARGET"|lolcat
+echo "*TUNGGU SEBENTAR..."|lolcat
+echo "*TUNGGU 5 DETIK..."|lolcat
+figlet "1"|lolcat
+figlet "2"|lolcat
+figlet "3"|lolcat
+figlet "4"|lolcat
+figlet "5"|lolcat
+clear
+echo "*TUNGGU SEBENTAR HAMPIR BERHASIL"|lolcat
+figlet "BERHASIL"|lolcat
+echo "*PROSES..."
+echo "[✓] BERHASIL ✓"|lolcat
+echo "###################################################"
+echo "TEMAN MU : P"|lolcat
+echo "PACAR MU : APA"|lolcat
+echo "TEMAN MU : UDAH PUNYA PACAR BELUM"|lolcat
+echo "PACAR MU : UDAH. EMNG KNP?"|lolcat
+echo "TEMAN MU : KAMU MAU GK JADI PACAR AKU "|lolcat
+echo "PACAR MU : HMM.."|lolcat
+echo "TEMAN MU : PLEASE"|lolcat
+echo "TEMAN MU : PUTUSIN AJH PACAR KAMUNYA.TRUS KAMU JADI PACARKU"|lolcat
+echo "PACAR MU : YAUDAH IYA NANTI AKU PUTUSIN PACAR AKU"|lolcat
+echo "TEMAN MU : IYH SAYANG"|lolcat
+echo "PACAR MU : OK SAYANG.LGI APA"|lolcat
+echo "TEMAN MU : LGI TIDURAN SAYANGG"|lolcat
+echo "PACAR MU : OHW.MAKAN BELUM SAYANG ?"|lolcat
+echo "TEMAN MU : UDAH SAYANG "|lolcat
+echo "PACAR MU : OHW"|lolcat
+echo "TEMEN MU : IYH SAYANG KU "|lolcat
+echo "###################################################>
+echo "PENGEN TAU SIAPA TEMEN MU YANG REBUT"|lolcat
+echo "PACAR MU "|lolcat
+echo "YANG REBUT PACAR MU ADALAH"|lolcat
+echo "INISIAL HURUF R ATAU A "|lolcat
+echo "HATI HATI KEBANYAKAN INISIAL HURUF R SAMA A FAKBOYY CAP KALENG KALENG"|lolcat
+echo "###################################################"
+echo "KALAU GAK PERCAYA TANYA PACARMU DIA SELINGKUH !"
+figlet " W A I T "|lolcat
+echo "HATI HATI DENGAN INISIAL HURUF R DAN A "|lolcat
+echo "MEREKA FAKBOY CAP KALENG KALENG "|lolcat
+echo "DAN PUNYA MANTAN BANYAK"|lolcat
+figlet "W A R I N G"|lolcat
